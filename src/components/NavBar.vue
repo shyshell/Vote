2b2a1a2e4fe494a93e1bb671739481aeaa7c15f2
Inffinite <louislaizer4@gmail.com>
2020-08-08 19:45:48 +0300

    :sparkles: Added a dropdown menu
    :bookmark: #todo. will add transitions on it

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bar">
+        <div class="name">
+            {{name}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+    props: {
+        path: String,
+        name: String
+    }
+}
+</script>
+
+<style lang="sass">
+    @import '../styles/nav-bar.scss';
+</style>
